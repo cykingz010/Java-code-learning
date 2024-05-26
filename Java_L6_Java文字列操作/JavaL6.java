bcfc88fcc25e4d9ef4a50ff1d3a827b7b54df5f0
Gakari <zihaoin1551@gmail.com>
2024-05-26 22:44:02 +0800

    文字列クラスの操作方法を習得

---
+++ Java_L6_Java文字列操作/JavaL6.java
@@ -0,0 +1,22 @@
+public class JavaL6 {
+    /* 文字列クラスの操作方法を習得し、Javaでの高度な文字列処理を実行します。 
+    検索や変更、フォーマットなど多彩なメソッドを修得します。*/
+
+   
+
+    public static void main(String[] args) {
+    
+    // StringBuilderを引数なしでインスタンス化して、変数sbに代入して
+    StringBuilder sb = new StringBuilder();
+    // 引数にABCを渡してStringBuilderをインスタンス化し、変数wordに代入して
+    StringBuilder word = new StringBuilder("ABC");
+    // 変数wordをtoStringメソッドで文字列に変換し、出力して
+    System.out.println(word.toString());
+    }
+
+
+
+
+  }
+
+  
