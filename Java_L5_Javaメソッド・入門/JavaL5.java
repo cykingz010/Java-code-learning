5bc2b84bdd4d19c233ce17dd2db51953ceee5ac3
Gakari <zihaoin1551@gmail.com>
2024-05-26 22:30:14 +0800

    Update JavaL5.java

--- Java_L5_Javaメソッド・入門/JavaL5.java
+++ Java_L5_Javaメソッド・入門/JavaL5.java
@@ -38,6 +38,26 @@ public class JavaL5 {
       return resultMessage;
     }
 
+    // int型の可変長引数numbersを受け取り、足し算の結果を格納する変数addNumに初期値0を代入し、
+    // 拡張for文で変数numを定義して足し算を行い、最後足し算の結果を出力するメソッドgetAdditionを定義して
+    public static void getAddition(int... numbers) {
+      int addNum = 0;
+      for (int num : numbers) {
+        addNum += num;
+      }
+      System.out.println(addNum);
+    }
+
+    // int型の可変長引数scoresを受け取り、足し算の結果を格納する変数addNumを定義し、
+    // 拡張for文で変数scoreを定義して足し算を行い、平均点をscoresの要素数で計算して返却するメソッドgetAverageを定義して
+    public static int getAverage(int... scores) {  // int 后面跟着的是实际参数？
+      int addNum = 0; 
+      for (int score : scores) {  //那这里的score又是做什么的呢？ 拡張for文で変数score 这个形容有点怪怪的
+        addNum += score;
+      }
+      return addNum / scores.length;
+    }
+
     public static void main(String[] args) {
       //定義したメソッドsayHelloを実行して
       sayHello();
@@ -63,6 +83,14 @@ public class JavaL5 {
       //定義したメソッドを使って「問題3: 」と「奇数です。」を結合して出力して
       System.out.println("問題3: " + evenOddDecision(21));
 
+      // 定義したメソッドgetAdditionの引数に1,2,3,4を渡して実行し、続いて5,6,7,8,9,10を渡して実行して
+      getAddition(1, 2, 3, 4);
+      getAddition(5, 6, 7, 8, 9, 10);
+
+      // 定義したメソッドgetAverageの引数に「60、80」を渡して「山田太郎: 」の後に結合して文字列を出力し、
+      // 続いて「80、70、90」を渡して「山田花子: 」の後に結合して文字列を出力して
+      System.out.println("山田太郎: " + getAverage(60, 80));
+      System.out.println("山田花子: " + getAverage(80, 70, 90));
 
     }
   }
