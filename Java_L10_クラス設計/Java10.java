3826406095876161aefa27724869d944d1e05b23
Gakari <zihaoin1551@gmail.com>
2024-05-27 22:11:51 +0800

    クラスの宣言とインスタンスの生成

---
+++ Java_L10_クラス設計/Java10.java
@@ -0,0 +1,16 @@
+/*  Javaでのオブジェクト指向プログラミングにおけるクラス設計の基礎を学ぶレッスンです。 
+クラスの作成、メンバフィールドやメソッドの定義方法から、オブジェクト間の関係性を理解し、クラス図の読み解き方を習得します。 */
+
+// SimpleClassクラスを宣言してください
+class SimpleClass {
+  
+}
+
+    public class Java10 {         //似乎编译的时候，根据public class 的名称，java文件也会变成相关的文件，否则会报错：找不到主类 xxx
+      public static void main(String[] args) {
+        // 作成したSimpleClassクラスのインスタンスを生成してください
+        SimpleClass simpleObject = new SimpleClass();
+      }
+    }
+    
+  
