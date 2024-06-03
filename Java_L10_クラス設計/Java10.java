e792be3080987c19d7e7ae14c9f2f1ea8deb6cfa
Gakari <zihaoin1551@gmail.com>
2024-06-03 22:39:41 +0800

    Update Java10.java

--- Java_L10_クラス設計/Java10.java
+++ Java_L10_クラス設計/Java10.java
@@ -26,15 +26,49 @@ class Restaurant {
 }
 
 class TaxCalculator {   //这个很关键，一定要搞清楚
-
   private double taxRate = 0.1;  //常态的0.1税率 （10%）
-
   // TaxCalculatorクラスにcalculateWithTaxメソッドを定義してください
   public double calculateWithTax(double amount) {
     // calculateWithTaxメソッドを実装してください
     double tax = amount * this.taxRate;  //税=税前amount *（1+tax），
     //但是这里的 this.taxRate是为了什么呢？是为了 double amount使用 上面常态的税率？
     return amount + tax;  //方法的返回值，返回的是含税总价
+  }
+}
+
+class Product {
+  private String productName;
+  // ProductクラスにてメンバフィールドproductNameを設定し、出力するコンストラクタを定義してください
+  public Product(String productName) {
+    this.productName = productName;
+    System.out.println(this.productName);
+  }
+}
+
+class student2 {
+  String name;
+  int age;
+  // Student2クラスにてメンバフィールドname,ageを設定するコンストラクタを定義してください
+  student2(String name, int age) {  //注意哈，结构体必须和类的名字一模一样，大小写敏感！
+    this.name = name;
+    this.age = age;
+  }
+}
+
+class Book {
+
+  // Bookクラスにメンバーフィールドtitle,priceをそれぞれString型,int型で宣言してください
+  String title;
+  int price;
+  // Bookクラスにてメンバフィールドtitle,priceを設定するコンストラクタを定義してください
+  public Book(String title, int price) {
+    this.title = title;
+    this.price = price;
+  }
+  // BookクラスのdisplayInfoメソッドをタイトルと価格を出力するメソッドに完成させてください
+  public void displayInfo() {
+    System.out.println("タイトル: " + this.title);
+    System.out.println("価格: " + this.price);
   }
 }
 
@@ -68,6 +102,18 @@ class TaxCalculator {   //这个很关键，一定要搞清楚
         // calculateWithTaxメソッドを実行してください
         TaxCalculator taxCalculator = new TaxCalculator();
         System.out.println(taxCalculator.calculateWithTax(100)); //这里用的是 对象.方法（参数）
+
+        // コンストラクタに引数 iPhone を渡して、Productクラスのインスタンスを生成し、Product型の変数productに代入
+        Product product = new Product("iPhone");
+
+        // 定義したコンストラクタを使用して、インスタンスを生成しよう
+        student2 student1 = new student2("Alice", 20);
+        student2 student2 = new student2("John", 21);
+        System.out.println(student2); //直接打印结构体的话，输出的是地址？
+
+        // Bookクラスのコンストラクタに引数を渡してインスタンスを生成し、displayInfoメソッドを使用し
+        Book book = new Book("Java入門", 2500);
+        book.displayInfo();
 
       }
     }
