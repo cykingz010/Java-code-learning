08ab60b2d02c63ab48d9413f633179dcad1efb77
Gakari <zihaoin1551@gmail.com>
2024-06-17 20:55:31 +0800

    ArrayListの操作を行って

--- Java_L8_Javaコレクション・リスト/JavaL8.java
+++ Java_L8_Javaコレクション・リスト/JavaL8.java
@@ -4,6 +4,8 @@
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class JavaL8 {
   public static void main(String[] args) {
@@ -83,5 +85,55 @@ public class JavaL8 {
     // 問題2. clearメソッドを使用して、リストintArrayの要素を全てまとめて削除して、出力してください
     intArray3.clear();
     System.out.println(intArray3);
+
+
+    /* 【演習】ArrayListの更新と削除を使って、問題を解いて */
+
+    ArrayList<String> greetArray3 = new ArrayList<>(
+      Arrays.asList("Good morning", "Hello", "Good morning", "Good")
+    );
+    // 問題1. リストgreetArray3の3つ目の要素をGood eveningに更新してください
+    greetArray3.set(2,"Good evening");
+    // 問題2. リストgreetArray3の最後の要素Goodを削除して、出力してください
+    greetArray3.remove(3);
+    System.out.println(greetArray3);
+
+    /* ArrayListの要素を調べる */
+    final String HELLO = "Hello";
+    ArrayList<String> greetArray4 = new ArrayList<>(
+      Arrays.asList("Good morning", "Hello", "Good evening")
+    );
+    // 問題1. indexOfメソッドを使用して、定数HELLOがリストgreetArrayの何番目に存在するか調べて出力してください
+    System.out.println(greetArray4.indexOf(HELLO));
+    // 問題2. containsメソッドを使用して、定数HELLOがリストgreetArrayに存在するか調べて出力してください
+    System.out.println(greetArray4.contains(HELLO));
+
+
+
+    /* ArrayListでサブリストの操作 */
+
+    ArrayList<Integer> intArray4 = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5));
+    List<Integer> subArray = new ArrayList<>();
+    // 問題1. subListメソッドを使用して、リストintArrayの1,2,3の要素をリストsubArrayに代入してください
+    subArray = intArray4.subList(0, 3); // 変数名.subList(始点インデックス, 終点インデックス+1);
+    // 問題2. setメソッドを使用して、リストsubArrayの3番目の要素を100に書き換えてください
+    subArray.set(2, 100);
+    // 問題3. リストintArray, subArrayを直接出力して値を確認してください
+    System.out.println(intArray4);
+    System.out.println(subArray);
+
+
+    
+    /* ArrayListの並び替え処理 */
+
+    ArrayList<String> rockArray = new ArrayList<>(Arrays.asList("Guns N' Roses", "Extreme", "AC/DC"));
+    ArrayList<Integer> intArray5 = new ArrayList<>(Arrays.asList(1, 3, 2, 5, 4));
+    // 問題1. Collections.sortメソッドを使用して、リストrockArrayを並び替えて出力してください
+    Collections.sort(rockArray);
+    System.out.println(rockArray);
+    // 問題2. Collections.sortメソッドを使用して、リストintArrayを並び替えて出力してください
+    Collections.sort(intArray5);
+    System.out.println(intArray5);
+
   }
 }
